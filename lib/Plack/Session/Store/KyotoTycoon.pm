dc9ac0545ed0858d4e8f0f334b435e7e0d6613f0
Lenz Gschwendtner <lenz@springtimesoft.com>
2013-05-24 16:08:08 +1200

    initial import

--- lib/Plack/Session/Store/KyotoTycoon.pm
+++ lib/Plack/Session/Store/KyotoTycoon.pm
@@ -1,47 +1,118 @@
 package Plack::Session::Store::KyotoTycoon;
 
-use 5.012;
-use Mouse;
+use 5.010;
+use Cache::KyotoTycoon;
+use Try::Tiny;
+use Storable qw/nfreeze thaw/;
+use MIME::Base64 qw(encode_base64 decode_base64);
 
-# ABSTRACT: 
+# ABSTRACT: Plack::Session storage for Kyoto Tycoon
 
 # VERSION
 
-use Data::Dump;
-
+use parent 'Plack::Session::Store';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Some::Module;
-    use Data::Dump 'dump';
-
-=head1 ATTRIBUTES
-
-=head2 attr
-
-=cut
-
-has 'attr' => (
-    is => 'rw',
-);
-
+See the docs for B<Plack::Session::Store::*> for details on how to use
+this.
 
 
 =head1 SUBROUTINES/METHODS
 
-=head2 method
+=head2 B<new ( %params )>
+
+Parameters are tycoon_server and tycoon_port. See the B<Cache::KyotoTycoon> and
+Kyoto Tycoon docs for details.
 
 =cut
 
-sub method {
-    my ($self) = @_;
+
+sub new {
+    my ($class, %params) = @_;
+
+    $params{tycoon} = _verify_tycoon_connection;
+
+    bless {%params} => $class;
 }
 
 
+=head2 B<fetch ( $session_id )>
+ 
+=cut
+
+sub fetch {
+    my ($self, $key) = @_;
+
+    my @data;
+    try {
+        @data = $self->{tycoon}->get($key);
+    }
+    catch {
+        $self->_verify_tycoon_connection;
+        @data = $self->{tycoon}->get($key);
+    };
+
+    if (defined($data[0])) {
+        my $sess = thaw(decode_base64($data[0]));
+        $sess->{__expire} = $data[1];
+        return $sess;
+    }
+    return;
+}
+
+=head2 B<store ( $session_id, $session )>
+
+=cut
+
+sub store {
+    my ($self, $key, $data, $expire) = @_;
+
+    $data = encode_base64(nfreeze($data));
+    $expire = 3600 unless $expire;
+    my $replace;
+    try {
+        $replace = $self->{tycoon}->replace($key, $data, $expire);
+    }
+    catch {
+        $self->_verify_tycoon_connection;
+        $replace = $self->{tycoon}->replace($key, $data, $expire);
+    };
+
+    return $self->{tycoon}->set($key, $data, $expire)
+        unless $replace;
+    return;
+}
+
+=head2 B<remove ( $session_id )>
+
+=cut
+
+sub remove {
+    my ($self, $key) = @_;
+
+    try { return $self->{tycoon}->remove($key); }
+    catch {
+        $self->_verify_tycoon_connection;
+        return $self->{tycoon}->remove($key);
+    }
+}
+
+sub _verify_tycoon_connection {
+    my ($self) = @_;
+
+    try { $self->{tycoon}->echo({ test => 1 }); }
+    catch {
+        $self->{tycoon} = Cache::KyotoTycoon->new(
+            timeout => 2,
+            exists $self->{tycoon_server}
+            ? (host => $self->{tycoon_server})
+            : (),
+            exists $self->{tycoon_port} ? (port => $self->{tycoon_port}) : (),
+        );
+    };
+    return;
+}
 
 =head1 BUGS
 
