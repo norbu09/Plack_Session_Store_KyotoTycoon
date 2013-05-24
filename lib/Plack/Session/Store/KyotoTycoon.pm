53036d2b5cfd0e46787b0262f7dd16ca501229e5
Lenz Gschwendtner <lenz@springtimesoft.com>
2013-05-24 15:55:28 +1200

    initial commit

---
+++ lib/Plack/Session/Store/KyotoTycoon.pm
@@ -0,0 +1,92 @@
+package Plack::Session::Store::KyotoTycoon;
+
+use 5.012;
+use Mouse;
+
+# ABSTRACT: 
+
+# VERSION
+
+use Data::Dump;
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Some::Module;
+    use Data::Dump 'dump';
+
+=head1 ATTRIBUTES
+
+=head2 attr
+
+=cut
+
+has 'attr' => (
+    is => 'rw',
+);
+
+
+
+=head1 SUBROUTINES/METHODS
+
+=head2 method
+
+=cut
+
+sub method {
+    my ($self) = @_;
+}
+
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests on GitHub's issue tracker L<https://github.com/<github_user>/Plack::Session::Store::KyotoTycoon/issues>.
+Pull requests welcome.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Plack::Session::Store::KyotoTycoon
+
+
+You can also look for information at:
+
+=over 4
+
+=item * GitHub repository
+
+L<https://github.com/<github_user>/Plack::Session::Store::KyotoTycoon>
+
+=item * MetaCPAN
+
+L<https://metacpan.org/module/Plack::Session::Store::KyotoTycoon>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Plack::Session::Store::KyotoTycoon>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Plack::Session::Store::KyotoTycoon>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+=over 4
+
+=item * Lenz Gschwendtner (@norbu09), for being an awesome mentor and friend.
+
+=back
+
+=cut
+
+1;  # End of Plack::Session::Store::KyotoTycoon
